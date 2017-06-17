e378359e7fbe78af27e15147e9133c3d9b6d0795
Taylor Fausak <taylor@fausak.me>
2017-06-17 10:02:37 -0500

    Use cd instead of pushd

--- external/install-stack.sh
+++ external/install-stack.sh
@@ -3,10 +3,9 @@ set -o errexit -o xtrace
 
 if test ! -f "$HOME/.local/bin/stack"
 then
-  pushd "$(mktemp --directory)"
+  cd "$(mktemp --directory)"
   curl --location 'https://www.stackage.org/stack/linux-x86_64-static' --output stack.tar.gz
   tar --extract --file stack.tar.gz --strip-components 1
   mkdir --parents "$HOME/.local/bin"
   mv stack "$HOME/.local/bin"
-  popd
 fi
